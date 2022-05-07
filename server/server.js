fde01c68d4a08a61df13cdad137a05f6e3a5979f
ilayWW <ilay@wnwd.com>
2022-05-07 15:43:12 +0300

    splitting app to 2 different package.json's

--- server/server.js
+++ server/server.js
@@ -1,27 +1,66 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const routes = require("./routes/routes.js");
+// const routes = require("./routes/routes.js");
 const gamesRoutes = require("./routes/games.route");
 const statsRoutes = require("./routes/stats.route");
 const app = express();
 const cors = require("cors");
 const path = require("path");
-const port = process.env.PORT || 3001;
-app.use(cors());
+const PORT = process.env.PORT || 3000;
+const isDev = process.env.NODE_ENV !== "production";
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-gamesRoutes(app);
-statsRoutes(app);
-// routes(app);
+// Multi-process to utilize all CPU cores.
+if (!isDev && cluster.isMaster) {
+  console.error(`Node cluster master ${process.pid} is running`);
 
-const publicPath = path.join(__dirname, "..", "public");
-app.use(express.static(publicPath));
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(publicPath, "index.html"));
-});
+  cluster.on("exit", (worker, code, signal) => {
+    console.error(
+      `Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`
+    );
+  });
+} else {
+  const app = express();
 
-const server = app.listen(port, function () {
-  console.log("app running on port.", server.address().port);
-});
+  // Priority serve any static files.
+  app.use(express.static(path.resolve(__dirname, "../react-ui/build")));
+
+  app.use(cors());
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  gamesRoutes(app);
+  statsRoutes(app);
+
+  // All remaining requests return the React app, so it can handle routing.
+  app.get("*", function (request, response) {
+    response.sendFile(
+      path.resolve(__dirname, "../react-ui/build", "index.html")
+    );
+  });
+
+  app.listen(PORT, function () {
+    console.error(
+      `Node ${
+        isDev ? "dev server" : "cluster worker " + process.pid
+      }: listening on port ${PORT}`
+    );
+  });
+}
+
+// // routes(app);
+
+// const publicPath = path.join(__dirname, "..", "public");
+// app.use(express.static(publicPath));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(publicPath, "index.html"));
+// });
+
+// const server = app.listen(port, function () {
+//   console.log("app running on port.", server.address().port);
+// });
