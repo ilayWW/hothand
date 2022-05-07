f1f5694d9f5066f4e4ea4f45bec635a436c99180
ilayWW <ilay@wnwd.com>
2022-05-07 16:29:58 +0300

    adding console.log for debugging

--- server/routes/games.route.js
+++ server/routes/games.route.js
@@ -3,6 +3,7 @@ const { fetchTeamsGames } = require("../services/nba.service");
 
 const gamesRouter = function (app) {
   app.get("/games/:season", async function (req, res) {
+    console.log("main games handler");
     const allTeamsData = await fetchTeamsGames(req.params.season);
     res.status(200).json(allTeamsData);
   });
